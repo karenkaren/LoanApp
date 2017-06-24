9cc988453d175cf1104ea7e41c4d81f9fa379774
王安帮 <wangab@desktop-v8164mv>
2017-06-24 11:41:58 +0800

    Initial Commit

---
+++ Loan/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Loan
+//
+//  Created by 王安帮 on 2017/6/24.
+//  Copyright © 2017年 FangRongTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ LoanTests/LoanTests.m
@@ -0,0 +1,39 @@
+//
+//  LoanTests.m
+//  LoanTests
+//
+//  Created by 王安帮 on 2017/6/24.
+//  Copyright © 2017年 FangRongTech. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LoanTests : XCTestCase
+
+@end
+
+@implementation LoanTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
